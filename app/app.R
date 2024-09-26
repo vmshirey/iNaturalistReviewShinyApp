f31c3fd88ad1378f12d69232db8b6ea6373d22fa
vmshirey <vmshirey@gmail.com>
2024-09-26 10:30:03 -0400

    - update app

---
+++ app/app.R
@@ -0,0 +1,34 @@
+library(shiny)
+library(shinydashboard)
+library(shinythemes)
+library(tidyverse)
+library(DT)
+
+iNat_dt <- data.table::fread("../data/Cleaned_Master_Database_Vaughn.csv") %>%
+  dplyr::select(Key, Language, `Publication Year`, Author, Title, Journal,
+                `Study Area Country`, `Study Area Region`, `Study Area Continent`, 
+                15:21, `Paper Topic`)
+
+ui <- shinydashboard::dashboardPage(
+  
+  shinydashboard::dashboardHeader(title = "iNaturalist Review Papers"),
+  shinydashboard::dashboardSidebar(width = 300, tags$p("\nWelcome to our Shiny app which will allow you to browse the papers included in our review of work that has utilized iNaturalist data.")),
+  shinydashboard::dashboardBody(
+    
+    shiny::fluidRow(
+      
+      DT::dataTableOutput("mytable")
+      
+    )
+  ), skin = "green"
+)
+
+server <- function(input, output){
+  
+  output$mytable <- DT::renderDataTable(iNat_dt,
+                                        filter = "top", 
+                                        options = list(pageLength = 10))
+  
+}
+
+shinyApp(ui = ui, server = server)
